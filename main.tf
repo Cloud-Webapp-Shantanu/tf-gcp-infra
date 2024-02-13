f468d388a23133cdafba9640a38c8e0c3624d826
tyagishantanu <tyagi.sha@northeastern.edu>
2024-02-12 23:13:27 -0500

    Vpc subnet initial code commit

---
+++ main.tf
@@ -0,0 +1,42 @@
+# Configure the Google Cloud provider
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+# Define the VPC with regional routing and no auto-created subnets
+resource "google_compute_network" "vpc" {
+  name                  = var.vpc_name
+  routing_mode          = "REGIONAL"
+  auto_create_subnetworks   = false
+}
+
+# Define the webapp subnet
+resource "google_compute_subnetwork" "webapp_subnet" {
+  name     = "webapp-subnet"
+  region  = var.region
+  ip_cidr_range = var.webapp_subnet_cidr
+  network  = google_compute_network.vpc.self_link
+}
+
+# Define the db subnet
+resource "google_compute_subnetwork" "db_subnet" {
+  name     = "db-subnet"
+  region  = var.region
+  ip_cidr_range = var.db_subnet_cidr
+  network  = google_compute_network.vpc.self_link
+}
+
+# Define a route to 0.0.0.0/0 for the webapp subnet
+resource "google_compute_route" "webapp_route" {
+  name            = "webapp-route"
+  network         = google_compute_network.vpc.name
+  dest_range      = "0.0.0.0/0"
+  next_hop_gateway = "global/gateways/default-internet-gateway"
+}
+
+# Create an internet gateway for the VPC
+resource "google_compute_router" "internet_gateway" {
+  name    = "internet-gateway"
+  network = google_compute_network.vpc.self_link
+}
---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "project_id" {
+  type = string
+  description = "The GCP project ID"
+}
+
+variable "region" {
+  type = string
+  description = "The GCP region where resources will be created"
+}
+
+variable "vpc_name" {
+  type = string
+  description = "The name of the VPC network"
+}
+
+variable "webapp_subnet_cidr" {
+  type = string
+  description = "The CIDR range for the webapp subnet"
+  default = "10.0.1.0/24"
+}
+
+variable "db_subnet_cidr" {
+  type = string
+  description = "The CIDR range for the db subnet"
+  default = "10.0.2.0/24"
+}
