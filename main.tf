2e393804108089d384beadeb37892eaa373e9f87
tyagishantanu <tyagi.sha@northeastern.edu>
2024-02-13 01:19:46 -0500

    Formatting changes

--- main.tf
+++ main.tf
@@ -6,32 +6,32 @@ provider "google" {
 
 # Define the VPC with regional routing and no auto-created subnets
 resource "google_compute_network" "vpc" {
-  name                  = var.vpc_name
-  routing_mode          = "REGIONAL"
-  auto_create_subnetworks   = false
+  name                    = var.vpc_name
+  routing_mode            = "REGIONAL"
+  auto_create_subnetworks = false
 }
 
 # Define the webapp subnet
 resource "google_compute_subnetwork" "webapp_subnet" {
-  name     = "webapp-subnet"
-  region  = var.region
+  name          = "webapp-subnet"
+  region        = var.region
   ip_cidr_range = var.webapp_subnet_cidr
-  network  = google_compute_network.vpc.self_link
+  network       = google_compute_network.vpc.self_link
 }
 
 # Define the db subnet
 resource "google_compute_subnetwork" "db_subnet" {
-  name     = "db-subnet"
-  region  = var.region
+  name          = "db-subnet"
+  region        = var.region
   ip_cidr_range = var.db_subnet_cidr
-  network  = google_compute_network.vpc.self_link
+  network       = google_compute_network.vpc.self_link
 }
 
 # Define a route to 0.0.0.0/0 for the webapp subnet
 resource "google_compute_route" "webapp_route" {
-  name            = "webapp-route"
-  network         = google_compute_network.vpc.name
-  dest_range      = "0.0.0.0/0"
+  name             = "webapp-route"
+  network          = google_compute_network.vpc.name
+  dest_range       = "0.0.0.0/0"
   next_hop_gateway = "global/gateways/default-internet-gateway"
 }
 
