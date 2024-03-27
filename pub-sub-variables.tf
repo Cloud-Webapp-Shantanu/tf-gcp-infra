ed3aa5f5992f887a169913a78e4c01865fb195da
tyagishantanu <tyagi.sha@northeastern.edu>
2024-03-27 15:44:23 -0400

    Pub sub changes

--- pub-sub-variables.tf
+++ pub-sub-variables.tf
@@ -7,3 +7,18 @@ variable "token_expiry" {
   type        = number
   description = "The expiry time of the token"
 }
+
+variable "verify_email_subscription" {
+  type        = string
+  description = "The name of the Pub/Sub subscription"
+}
+
+variable "message_retention_duration" {
+  type        = string
+  description = "The duration for which the message is retained"
+}
+
+variable "ack_deadline_seconds" {
+  type        = number
+  description = "The deadline for acknowledging the message"
+}
--- pub-sub.tf
+++ pub-sub.tf
@@ -1,3 +1,11 @@
 resource "google_pubsub_topic" "verify_email" {
-  name = var.pubsub_topic_name
+  name                       = var.pubsub_topic_name
+  message_retention_duration = var.message_retention_duration
+}
+
+resource "google_pubsub_subscription" "verify_email_subscription" {
+  name                       = var.verify_email_subscription
+  topic                      = google_pubsub_topic.verify_email.name
+  ack_deadline_seconds       = var.ack_deadline_seconds
+  message_retention_duration = var.message_retention_duration
 }
